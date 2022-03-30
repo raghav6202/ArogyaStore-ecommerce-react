ae9ba333415b1bb34e98c5cfba0731774b020396
Raghav Chandra V S <86127392+raghav6202@users.noreply.github.com>
2022-03-30 18:37:07 +0530

    docs: update

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,29 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "Medicenes",
     description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+      "Get Medicenes at affordable prices.Shop Now and get discounts upto 20%",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "Tests",
     description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+      "Diagnostic tests done at youe doorstep.Book Now and get discounts upto 30%",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "Med-Tech",
     description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+      "Get premium quality health monitoring devices.Book Now and get discounts upto 30%",
   },
+
+  {
+    _id: uuid(),
+    categoryName: "Rehab",
+    description:
+      "Get natural health products and exercise equipments.Book Now and get discounts upto 20%",
+  },
+
+
 ];
